e3d7ebee973aad31fe4551d13e1ef3045c81c2b9
xiaowan.chen <xiaowan.chen@weimob.com>
2018-12-11 10:47:28 +0800

    ll

---
+++ src/NodeCard.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './style.css';
+/**
+ *
+ *
+ * @class NodeCeard
+ * @extends {Component}
+ */
+class NodeCeard extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+    
+    }
+
+  }
+
+  render() {
+    return ( //return的内容是页面要展示的东西。
+        <span>hello react</span>
+    );
+  }
+}
+
+export default NodeCard;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';//引入的目的是识别react这个语法
 import ReactDOM from 'react-dom';//可以让组件挂在到页面的Dom节点上
 import './index.css';
 // App组件，大写字母一般是组建
-import Todolist from './Todolist';
+import NodeCard from './NodeCard';
 
-ReactDOM.render(<Todolist />, document.getElementById('root'));//把组件渲染到指定的标签下
+ReactDOM.render(<NodeCard />, document.getElementById('root'));//把组件渲染到指定的标签下
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
