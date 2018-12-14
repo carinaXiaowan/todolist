6c11730186ae4c21fdfa0708a8c055d36a5dfe28
xiaowan.chen <xiaowan.chen@weimob.com>
2018-12-14 10:19:51 +0800

    ww

--- src/Todoitem.js
+++ src/Todoitem.js
@@ -6,9 +6,9 @@ class Todoitem extends Component {
         this.handleDelete = this.handleDelete.bind(this);
         console.info(this.props)
     }
-    handleDelete(){
+    handleDelete(){ 
         const { deletes , index} = this.props;
-        deletes({index});
+        deletes({index}); //回调父元素属性deletes绑定的方法。（向父元素传值要删除的索引号index）
     }
     render(){
         const { content } = this.props;  //解构赋值 this.props = {content: "learning-react", index: 0, delete: ƒ}
